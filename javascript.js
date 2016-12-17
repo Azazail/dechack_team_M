855d32d2dcfe392d511d41925b20d146b4043bc4
Arkim Edmond <aedmond@westinghousehs.org>
2016-12-17 20:52:16 +0000

    latest  commit

--- javascript.js
+++ javascript.js
@@ -57,4 +57,36 @@ function createPie(dataElement, pieElement) {
 }
 createPie(".pieID.legend", ".pieID.pie");
 
+
+var negative=[27756, 17482, 17394, 146, 10, 2247, 4193, 2536, 50856, 33348];
+var positive=[244330, 9796, 12123, 1895, 173, 2247, 163970, 11351, 32202, 427];
+
+var array =["https://www.youtube.com/embed/XjJQBjWYDTs", "https://www.youtube.com/embed/WWTRwj9t-vU", "https://www.youtube.com/embed/pANqQHv4e3w","https://www.youtube.com/embed/ZKHS4PIVRxg", "https://www.youtube.com/embed/OxhdrVP_L_8", "https://www.youtube.com/embed/Ghl19lJJ_mA", "https://www.youtube.com/embed/XpaOjMXyJGk","https://www.youtube.com/embed/9ilSeJ6B5ro", "https://www.youtube.com/embed/vHGPbl-werw", "https://www.youtube.com/embed/uM19yt7MbN8"];
+var index =0;
+$("#next").click(function(){
+  console.log ('hi')
+    index++;
+    $(".positive").show();
+    $("#progress").animate({
+        width:index*10+"%"},1000);
+    $("#bar").hide();
+    var animation =1500;
+    var video= $("iframe");
+    $( "iframe" ).slideUp( animation, function(){  
+    video.attr('src', array[index]);
+    });
+    $( "iframe" ).slideDown(animation); 
+     $("#positive").html(positive[index]);
+   $("#negative").html(negative[index]);
+   createPie(".pieID.legend", ".pieID.pie");
+   if (index > 9){
+       window.location="https://azazail.github.io/dechack_team_M/success.html"
+   }
+});
+
+$("#startbutton").click(function(){
+    $("#pic1").hide();
+    $("#start").hide();
+    $("#whole").show();
+});
 });
